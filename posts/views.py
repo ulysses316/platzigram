9a5c56cc5a05f98dd1b3fc1c376b6fdfb4a9d9fb
ulysses316 <ulises316@live.com.mx>
2018-10-26 12:03:02 -0500

    Se agrego el render template y agregamos nuestros datos del request al html

--- posts/views.py
+++ posts/views.py
@@ -7,25 +7,23 @@ posts = [
     {
         "name" : "Como estudiar programacion a nivel intermedio",
         "user" : "Ulysses",
-        "timeStamp": datetime.now().strftime('%b %dth, %Y - %H:%M hrs')
+        "timeStamp": datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        "img": "https://images.unsplash.com/photo-1460626399219-57a00a2361cb?ixlib=rb-0.3.5&s=7486c030f52264deaa23eadbeb92f48b&auto=format&fit=crop&w=750&q=80"
     },
     {
         "name" : "¿Son inportantes las bases?",
         "user" : "Ulises",
-        "timeStamp": datetime.now().strftime('%b %dth, %Y - %H:%M hrs')
+        "timeStamp": datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        "img": "https://images.unsplash.com/photo-1460750860062-82a52139a0d6?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=ad6f8d20e4b6da905c5bb18ea656ce60&auto=format&fit=crop&w=750&q=80"
     },
     {
         "name" : "Naruto vs Sasuke",
         "user" : "Ulysses316",
-        "timeStamp": datetime.now().strftime('%b %dth, %Y - %H:%M hrs')
+        "timeStamp": datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        "img": "https://images.unsplash.com/photo-1453668069544-b8dbea7a0477?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=7bdf2569bc30ad7aacdf645f834c766c&auto=format&fit=crop&w=750&q=80"
     }
 ]
 
 # Create your views here.
 def lists_posts(request):
-    contenido =[]
-    for post in posts:
-        contenido.append("""<p><strong>{name}<strong><p>
-                            <p><small>{user}<i> {timeStamp}</i></small></p>
-                            """.format(**post))
-    return HttpResponse('<br>' .join(contenido))
+    return render(request, "feed.html", {"posts" : posts})
